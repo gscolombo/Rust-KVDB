5f0586769309f637db266e417c7abe55f41d97ef
Gabriel S. Colombo <gscolombo404@gmail.com>
2025-10-25 16:21:36 -0300

    Protótipo de criação de registro em formato binário customizado

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+mod records;
+
+use records::{create_record, serialize_record};
+use std::env;
+use std::fs;
+use std::io::Write;
+
+fn main() -> std::io::Result<()> {
+    let args: Vec<String> = env::args().collect();
+
+    let key = &args[1];
+    let path = &args[2];
+
+    println!("Creating record for file in {path}...");
+    let rec = create_record(key, path);
+
+    let fname = key.clone() + ".kvdb";
+    let mut db = fs::File::create(fname).expect("Error creating database file.");
+    
+    db.write_all(&serialize_record(rec))?;
+    Ok(())
+}
